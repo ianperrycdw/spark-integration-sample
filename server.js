1ba0c89ff6335b86bec754d4faa8009f83a42f49
ianperrycdw <32092683+ianperrycdw@users.noreply.github.com>
2017-09-19 10:52:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -230,7 +230,7 @@ function oauthFlowCompleted(access_token, res) {
         // Uncomment to send feedback via static HTML code 
         //res.send("<h1>OAuth Integration example for Cisco Spark (static HTML)</h1><p>So happy to meet, " + json.displayName + " !</p>");
         // OR leverage an EJS template
-        var str = read(join(__dirname, '/www/list-rooms.ejs'), 'utf8');
+        var str = read(join(__dirname, '/togofurther/list-rooms.ejs'), 'utf8');
         var compiled = ejs.compile(str)({ "title": json.title });
         res.send(compiled);
     });
